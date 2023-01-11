7524d1935584151793a7f0cba2a8b2610e9497b7
devcatalagamer <contact.elcatalagamer@gmail.com>
2023-01-11 22:42:35 +0100

    footer, contact y nightmode

--- JS/Script_Inicio.js
+++ JS/Script_Inicio.js
@@ -5,6 +5,32 @@ window.onload = function () {
 
 document.oncontextmenu = function(){return false}
 
+const btnAbrirModal = document.querySelector("#btn-Abrir-Modal");
 
+const btnCerrarModal = document.querySelector("#btn-Cerrar-Modal");
 
+const modal = document.querySelector("#modal");
 
+btnAbrirModal.addEventListener("click",()=>{
+  modal.showModal()
+});
+
+btnCerrarModal.addEventListener("click",()=>{
+  modal.closest();
+});
+
+const toggle = document.getElementById('toogleDark');
+const body = document.querySelector('body');
+
+toggle.addEventListener('click', function(){
+  this.classList.toggle('bi-moon');
+  if(this.classList.toggle('bi-brightness-high-fill')){
+    body.style.background = 'white';
+    body.style.color = 'black';
+    body.style.transition = '2s'
+  }else{
+    body.style.background = 'black';
+    body.style.color = 'white';
+    body.style.transition = '2s';
+  }
+})
