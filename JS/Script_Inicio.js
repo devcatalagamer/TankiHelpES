1b307e1b1371f97f5e9e03d95b3a687e4bb5c834
devcatalagamer <contact.elcatalagamer@gmail.com>
2023-01-14 13:01:50 +0100

    cambio tipografia general y menu con fondo

--- JS/Script_Inicio.js
+++ JS/Script_Inicio.js
@@ -22,15 +22,15 @@ btnCerrarModal.addEventListener("click",()=>{
 const toggle = document.getElementById('toogleDark');
 const body = document.querySelector('body');
 
-toggle.addEventListener('click', function(){
-  this.classList.toggle('bi-moon');
-  if(this.classList.toggle('bi-brightness-high-fill')){
-    body.style.background = 'white';
-    body.style.color = 'black';
-    body.style.transition = '2s'
-  }else{
-    body.style.background = 'black';
-    body.style.color = 'white';
-    body.style.transition = '2s';
-  }
-})
+// toggle.addEventListener('click', function(){
+//   this.classList.toggle('bi-moon');
+//   if(this.classList.toggle('bi-brightness-high-fill')){
+//     body.style.background = 'white';
+//     body.style.color = 'black';
+//     body.style.transition = '2s'
+//   }else{
+//     body.style.background = 'black';
+//     body.style.color = 'white';
+//     body.style.transition = '2s';
+//   }
+// })
