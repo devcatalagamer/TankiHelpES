819e5fba3b5040207e1e061f9484f327dedcc8d3
devcatalagamer <102853165+devcatalagamer@users.noreply.github.com>
2023-05-01 16:57:09 +0200

    new upload

---
+++ JavaScript/Program.js
@@ -0,0 +1,9 @@
+/*preguntas y respuestas*/
+
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq) => {
+    faq.addEventListener("click", () => {
+        faq.classList.toggle("active")
+    });
+});
